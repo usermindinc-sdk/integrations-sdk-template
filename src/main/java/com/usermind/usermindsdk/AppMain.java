ebaccb55c94c9f1be9b18c8452fb6422711a1dc7
Evan Reynolds <evan@usermind.com>
2018-03-27 11:12:01 -0700

    Config file update

--- src/main/java/com/usermind/usermindsdk/AppMain.java
+++ src/main/java/com/usermind/usermindsdk/AppMain.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 
 public class AppMain {
 
+    //TODO - move arguments to code so it runs with no arguments
+    //TODO - search for config file - same directory, /etc/usermind
     public static void main(String[] args) throws Exception {
 
         //Swagger throws a lot of reflection errors looking for APIs ... this just turns those off
