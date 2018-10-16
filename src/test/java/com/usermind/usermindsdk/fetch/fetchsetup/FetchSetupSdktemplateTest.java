dbc7334c6eeffecef69c20c888776366189f0e88
Evan Reynolds <evan@usermind.com>
2018-10-15 23:51:24 -0700

    Fixed build issue

--- src/test/java/com/usermind/usermindsdk/fetch/fetchsetup/FetchSetupSdktemplateTest.java
+++ src/test/java/com/usermind/usermindsdk/fetch/fetchsetup/FetchSetupSdktemplateTest.java
@@ -1,8 +1,10 @@
 package com.usermind.usermindsdk.fetch.fetchsetup;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
+
+import com.usermind.usermindsdk.fetch.metadatafetch.EntityInformationSdktemplate;
 import org.junit.jupiter.api.Test;
 import com.usermind.usermindsdk.TestBase;
 import com.usermind.usermindsdk.TestClassFactory;
@@ -10,15 +12,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
-
 @ExtendWith(MockitoExtension.class)
 class FetchSetupSdktemplateTest extends TestBase {
     private FetchSetupSdktemplate fetchSetup;
 
     @BeforeEach
     void setUp() throws Exception {
-        fetchSetup = new FetchSetupSdktemplate(restTemplate, objectMapper);
+        fetchSetup = new FetchSetupSdktemplate(restTemplate, objectMapper, new EntityInformationSdktemplate());
     }
 
     @Test
