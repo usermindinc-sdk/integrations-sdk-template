ee4fb67f1a5b18c3bd73c3ad946fc87e58212788
Evan Reynolds <evan@usermind.com>
2019-08-11 03:17:34 -0700

    Bits to start supporting batched actions

--- src/main/java/com/usermind/usermindsdk/actions/SdktemplateCreateEntityAction.java
+++ src/main/java/com/usermind/usermindsdk/actions/SdktemplateCreateEntityAction.java
@@ -5,12 +5,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component("CreateEntity")
 public class SdktemplateCreateEntityAction implements ActionHandler<SdktemplateConnectionData, SdktemplateCreateEntityInput>  {
     private static final Logger LOGGER = LoggerFactory.getLogger(SdktemplateCreateEntityAction.class);
 
+
     @Override
-    public void runAction(SdktemplateConnectionData sdktemplateConnectionData, String s, SdktemplateCreateEntityInput sdktemplateCreateEntityInput) throws Exception {
+    public int getBatchSize() {
+        return 1;
+    }
+
+    @Override
+    public void runAction(SdktemplateConnectionData sdktemplateConnectionData, String s, List<SdktemplateCreateEntityInput> sdktemplateCreateEntityInput) throws Exception {
 
         //Throw new SDKActionFailedException if the action fails.
 
