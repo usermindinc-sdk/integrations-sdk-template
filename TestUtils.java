8c13ed6ac9c4855ca3c8a401a0c68913cff6f1a2
Evan Reynolds <evan@usermind.com>
2020-12-10 16:39:57 -0800

    Updating SDK infrastructure

---
+++ TestUtils.java
@@ -0,0 +1,34 @@
+package com.usermind.usermindsdk;
+
+import com.usermind.tracking.TrackingLog;
+import com.usermind.usermindsdk.normalization.Normalizer;
+
+import java.io.*;
+
+public class TestUtils {
+
+    private TestUtils() {}
+
+    public static BufferedReader stringToBufferedReader(String input) {
+        Reader reader = new StringReader(input);
+        return new BufferedReader(reader);
+    }
+
+    public static String normalize(String entityName, String dataToNormalize, Normalizer normalizer) throws IOException {
+        String flattenedData;
+        try (Reader entityReader = new StringReader(dataToNormalize);
+             BufferedReader bufferedReader = new BufferedReader(entityReader);
+             StringWriter stringWriter = new StringWriter();
+             StringWriter stringWriterForInvalidRecords = new StringWriter();
+             BufferedWriter bufferedWriterForInvalidRecords = new BufferedWriter(stringWriterForInvalidRecords);
+             BufferedWriter bufferedWriter = new BufferedWriter(stringWriter);) {
+
+            normalizer.normalizeData(entityName, bufferedReader, bufferedWriter, bufferedWriterForInvalidRecords);
+            bufferedWriter.flush();
+            flattenedData = stringWriter.toString();
+        }
+        return flattenedData;
+    }
+
+
+}
